c5646c66056ab30615c9e9b27e0519f72d53af5b
MightyCharm <109044408+MightyCharm@users.noreply.github.com>
2025-08-20 16:28:12 +0200

    feat(gameboard): implement module with input validation and safe access

--- script.js
+++ script.js
@@ -1,3 +1,68 @@
-const para = document.getElementById("para");
+// 2.
+// - You’re going to store the gameboard as an array inside of a Gameboard object
+// - Your players are also going to be stored in objects
+// - object to control the flow of the game itself
+// 2.1
+// - have as little global code as possible
+// - tucking as much as you can inside factories
+// - if only single instance of something is need, like gameboard, displayControlle...)
+//   wrap factory inside IIFE (module pattern)
+// 2.2
+// - think carefully about where each bit of logic should reside.
+// - each little piece of functionality should be able to fit in
+//   the game, player or gameboard objects. 
+// - take care to put them in “logical” places.
+// 2.3 building a house from the inside out
+// 3.
+// - getting a working game in the console first
+// - make sure to include logic that checks when the game is over
+// - avoid thinking about DOM and your HTML/CSS until game is working
+// - make sure the game is running smoothly
 
-para.textContent = "Hello World!";
+
+// 4. one you have a working game -> create object that will handle display/DOM logic....
+
+const gameboard = (function() {
+    // min: 0/0 max 2/2
+    const board = 
+    [
+        ["", "", ""],
+        ["", "", ""],
+        ["", "", ""]
+    ];
+
+    const getBoard = () => {
+        // returns a copy of board, so nothing can be changed
+        return board.slice();
+    }
+
+    const setCell = ((row, column, playerSign) => {
+        if(row === undefined ||  column === undefined || playerSign === undefined) {
+            console.log("missing-input");
+            return;
+        }
+        if( (row < 0 || row > 2) || (column < 0 || column > 2)) {
+            console.log("invalid-coords");
+            return;
+        }
+        if(board[row][column] === "") {
+            board[row][column] = playerSign;
+            console.log("success");
+            return;
+        }
+        console.log("cell-taken"); 
+    })
+
+    return { getBoard, setCell };
+})();
+
+
+
+gameboard.setCell(1, 1, "X");
+console.log(gameboard.getBoard());
+gameboard.setCell(2, 1, "O");
+console.log(gameboard.getBoard());
+gameboard.setCell(1, 1, "X")
+
+gameboard.setCell(5, 5, 'X');
+gameboard.setCell();
